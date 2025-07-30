9b1adb9730007eace4c7ad30a3f4b41c27e7d247
Terry Yegon <yegonterry618@gmail.com>
2025-07-30 09:41:42 +0300

    assignment done

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,34 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-import ProjectList from "./ProjectList";
 import user from "../data/user";
+import ProjectList from "./ProjectList";
+
+const projectData = [
+  {
+    id: 1,
+    name: "Portfolio Website",
+    about: "A personal portfolio site.",
+    technologies: ["HTML", "CSS", "JavaScript"]
+  },
+  {
+    id: 2,
+    name: "To-Do App",
+    about: "A simple to-do app using React.",
+    technologies: ["React", "CSS"]
+  }
+];
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <Home />
+      <About />
+      <ProjectList projects={projectData} />
     </div>
   );
 }
+
 
 export default App;
